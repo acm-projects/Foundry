74fe1600faf4aa6c273c01067039c80c2dd687d0
Enaya Jawed <jawedenaya@gmail.com>
2025-10-03 21:17:02 -0500

    remove dashboard and replace with settings page

--- frontend/app/(pages)/workflows/[projectId]/settings/page.js
+++ frontend/app/(pages)/workflows/[projectId]/settings/page.js
@@ -1,10 +1,10 @@
 
 
-export default function DashboardPage({ params }) {
+export default function SettingsPage({ params }) {
   return (
     <div className="pt-8 pb-8">
       <h1 className="text-2xl font-bold mt-4">
-        Dashboard for Project {params.projectId}
+        Settings for Project {params.projectId}
       </h1>
 
     </div>
