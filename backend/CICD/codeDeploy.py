6ce61cd6b754186f460a47088187dbb45c40287f
Enaya Jawed <jawedenaya@gmail.com>
2025-10-26 20:12:15 -0500

    create codeDeploy script

--- backend/CICD/codeDeploy.py
+++ backend/CICD/codeDeploy.py
@@ -1,18 +1,25 @@
-import boto3 
+import boto3
 
-#out_file is the parameter representing the file to be deployed
-def codeDeploy(out_file): 
-
-
+def codeDeploy():
     code_deploy = boto3.client("codedeploy", region_name="us-east-1")
 
-
-    try: 
+    try:
         response = code_deploy.create_deployment(
-
-            #fill in parameters
+            applicationName="MyApp",
+            deploymentGroupName="MyDeploymentGroup",
+            revision={
+                'revisionType': 'S3',
+                's3Location': {
+                    'bucket': 'foundry-codebuild-zip',
+                    'key': 'artifacts/efrain-grubs/ai-vs-human-written-text-main.zip',
+                    'bundleType': 'zip'
+                }
+            },
+            deploymentConfigName='CodeDeployDefault.AllAtOnce',
+            description='Deploying latest build to EC2'
         )
 
+        print("Deployment started:", response['deploymentId'])
 
-    except Exception as e: 
+    except Exception as e:
         print(f"Failed to trigger CodeDeploy: {e}")
