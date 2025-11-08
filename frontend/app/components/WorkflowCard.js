89d47de37999120dc40ff0b1b771376379d7f737
Enaya Jawed <jawedenaya@gmail.com>
2025-11-07 19:05:37 -0600

    something

--- frontend/app/components/WorkflowCard.js
+++ frontend/app/components/WorkflowCard.js
@@ -1,40 +1,66 @@
+import WorkflowCard from "@/app/components/WorkflowCard";
 import { Card , CardHeader, CardTitle, CardDescription, CardContent} from '@/app/components/ui/card'
-import DeploymentStatus from "@/app/components/DeploymentStatus";
-import Link from 'next/link'; // Import Link
+import { Plus } from "lucide-react";
+import { useEffect, useState} from "react"; 
+import { useSession } from "next-auth/react"; 
+import axios from "axios"
+import Link from "next/link";
+export default function WorkflowGrid() {
 
-export default function WorkflowCard({title, description, status, created_at}){
-console.log("workflow card props",created_at)
+  const data = useSession()
+  const[projects,setProjects] = useState([])
+
+
+useEffect(() => {
+  
+const get_builds = async () => { 
+  try { 
+    const response = await axios.get(`http://localhost:8000/builds`,{params: {id: data.data?.user?.id}});
+
+
+    console.log("backend response",response)
+
+
+    setProjects(response.data)  
+  }catch(err) { 
+  
+  
+    console.log("error",err)
+  }
+  
+}
+
+
+get_builds()
+
+
+
+},[data])
+
+
   return (
-    <div>
-      <Card className="group flex flex-col justify-between flex-1 min-w-88 max-w-88 flex-grow min-h-60 
-  bg-gray-100 backdrop-blur-md shadow-lg rounded-2xl
-  transition duration-200 hover:scale-[1.02] hover:cursor-pointer">
-        <CardHeader className="gap-4">
-          <div className="flex items-center justify-between">
-            <CardTitle className="transition-colors truncate pr-2">
-              {title}
-            </CardTitle>
-            <DeploymentStatus status={status}/>
-          </div>
+    <>
+      {projects.map((proj) => {
+  const formattedDate = new Date(proj.created_at).toLocaleString("en-US", {
+    dateStyle: "medium",
+    timeStyle: "short",
+  });
 
-          <CardDescription>
-            {description}
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className="flex justify-between max-w-7xl mx-auto">
-         
-            <p className=" text-neutral-500 text-right">{created_at}</p>
-          </div>
-        </CardContent>
-      </Card>
+  return (
+    <Link  href={`/builds/${proj.build_id}/canvas`}>
+    <WorkflowCard
+      key={proj.id}
+      title={proj.project_name || "Untitled"}
+      description={proj.description}
+      status={proj.status || "inactive"}
+      created_at={formattedDate}
+    />
+
+</Link>
   );
+})}
 
-  return (
-    <div>
-        <Link href={href}>
-            {CardContentComponent}
-        </Link>
-    </div>
-  )
+    </>
+
+  );
 }
